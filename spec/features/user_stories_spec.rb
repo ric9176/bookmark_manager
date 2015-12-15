0255ff1d26c6b4799db7bccbc07af39e32994343
Richard Moss <richard@englishtutorslive.com>
2015-12-15 17:14:40 +0000

    datamapper setup

---
+++ spec/features/user_stories_spec.rb
@@ -0,0 +1,13 @@
+feature "display a list of links" do
+  scenario "the user is shown a list of links" do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+
+    visit '/'
+
+    expect(page.status_code).to eq 200
+
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
+  end
+end
