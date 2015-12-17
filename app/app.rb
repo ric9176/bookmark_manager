f6e48b48f8371ddff34e75bf1428cc9b4e5f164d
Richard Moss <richard@englishtutorslive.com>
2015-12-17 16:27:19 +0000

    adds multiple tags to controller

--- app/app.rb
+++ app/app.rb
@@ -18,14 +18,20 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url],
-    title: params[:title])
-    tag = Tag.create(name: params[:tags])
-    link.tags << tag
+    link = Link.new(url: params[:url], title: params[:title])
+    params[:tags].split.each do |tag|
+      link.tags << Tag.create(name: tag)
+    end
     link.save
-    p params
     redirect '/links'
   end
+
+  get '/tag/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :index
+  end
+
 
   # start the server if ruby file executed directly
   run! if app_file == $0
--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -1,5 +1,3 @@
-
-
 feature "display a list of links" do
   scenario "the user is shown a list of links" do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
@@ -36,5 +34,42 @@ feature 'Adding tags' do
     click_button 'Create New'
     link = Link.first
     expect(link.tags.map(&:name)).to include('education')
+  end
+end
+
+
+feature 'Tagging things' do
+  before(:each) do
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
+    Link.create(url: 'http://www.google.com', title: 'Google', tags: [Tag.first_or_create(name: 'search')])
+    Link.create(url: 'http://www.zombo.com', title: 'This is Zombocom', tags: [Tag.first_or_create(name: 'bubbles')])
+    Link.create(url: 'http://www.bubble-bobble.com', title: 'Bubble Bobble', tags: [Tag.first_or_create(name: 'bubbles')])
+  end
+
+
+  scenario 'can view links that are tagged' do
+    visit '/tag/bubbles'
+
+
+
+    expect(page.status_code).to eq (200)
+    within 'ul#links' do
+      expect(page).not_to have_content('Makers Academy')
+      expect(page).not_to have_content('Code.org')
+      expect(page).to have_content('This is Zombocom')
+      expect(page).to have_content('Bubble Bobble')
+    end
+  end
+end
+
+feature 'multiple tags' do
+  scenario 'I can add multiple tags to a new link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.makersacademy.com/'
+    fill_in 'title', with: 'Makers Academy'
+    fill_in 'tags', with: 'education ruby'
+    click_button 'Create New'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education', 'ruby')
   end
 end
