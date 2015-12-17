62dc923d9a0a9b3f1f40bd0b74d5f591aaba20b5
Richard Moss <richard@englishtutorslive.com>
2015-12-17 11:03:16 +0000

    moved views folder

--- spec/features/user_stories_spec.rb
+++ spec/features/user_stories_spec.rb
@@ -23,3 +23,16 @@ feature "add a link" do
     end
   end
 end
+
+feature 'Adding tags' do
+  scenario 'can add a single tag to a new link' do
+    visit '/links/new'
+    fill_in 'url', with: 'http://www.englishtutorslive.com'
+    fill_in 'title', with: 'ETL'
+    fill_in 'tags', with: 'education'
+
+    click_button 'Create link'
+    link = Link.first
+    expect(link.tags.map(&:name)).to include('education')
+  end
+end
