e692ce1d1a453cc8ee191875b93500749989fe3c
Andrew Burnie <apburnie@hotmail.co.uk>
2015-12-18 12:11:43 +0000

    Welcomes new users

--- app/app.rb
+++ app/app.rb
@@ -4,17 +4,20 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+
+  enable :sessions
   get '/' do
     redirect '/links'
   end
 
   get '/links' do
+    @user = User.get(session[:id])
     @links = Link.all
-    erb(:index)
+    erb(:'links/index')
   end
 
   get '/links/new' do
-    erb :form
+    erb :'links/new'
   end
 
   post '/links' do
@@ -29,7 +32,13 @@ class BookmarkManager < Sinatra::Base
   get '/tag/:name' do
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
-    erb :index
+    erb :'links/index'
+  end
+
+  post '/users' do
+    user = User.create(username: params[:username], password: params[:password])
+    session[:id] = user.id
+    redirect '/links'
   end
 
 
--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,6 +3,7 @@ require 'dm-postgres-adapter'
 
 require_relative '../lib/tag.rb'
 require_relative '../lib/link.rb'
+require_relative '../lib/user.rb'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
