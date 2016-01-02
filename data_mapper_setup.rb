0071002e47e7e2b922de9a71805a13c47c6e78e4
ric9176 <richard@englishtutorslive.com>
2016-01-02 23:08:37 +0000

    moves data_mapper_setup

--- data_mapper_setup.rb
+++ data_mapper_setup.rb
@@ -2,9 +2,9 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 
 
-require_relative '../lib/tag.rb'
-require_relative '../lib/link.rb'
-require_relative '../lib/user.rb'
+require_relative './lib/tag.rb'
+require_relative './lib/link.rb'
+require_relative './lib/user.rb'
 
 DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
