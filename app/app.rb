f14d8f326629fc4c870b87c012834bda527825d3
ric9176 <richard@englishtutorslive.com>
2016-01-02 19:33:32 +0000

    adds login functionality

--- app/app.rb
+++ app/app.rb
@@ -48,7 +48,7 @@ class Bookmark < Sinatra::Base
       session[:user_id] = @user.id
       redirect to('/')
     else
-      flash.now[:notice] = "Password and confirmation password do not match"
+      flash.now[:errors] = @user.errors.full_messages
       erb :'users/new'
     end
   end
@@ -58,6 +58,22 @@ class Bookmark < Sinatra::Base
      @current_user ||= User.get(session[:user_id])
    end
   end
+
+  get '/sessions/new' do
+    erb :'sessions/new'
+  end
+
+  post '/sessions' do
+
+  user = User.authenticate(params[:email], params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect to('/')
+  else
+    flash.now[:errors] = ['The email or password is incorrect']
+    erb :'sessions/new'
+  end
+end
 
 
   # start the server if ruby file executed directly
